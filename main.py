beb233d18f19306594eac8759eadf9dad8ee6fa1
brypy220 <brypy220@gmail.com>
2022-11-27 21:05:10 -0500

    Initial Commit

---
+++ main.py
@@ -0,0 +1,6 @@
+print("Welcome to Band Name Generator!\n")
+city = input("What's the name of the city you grew up in?\n")
+print(city)
+pet_name = input("What's your pet's name?\n")
+print(pet_name)
+print(f"Your band name could be {city} {pet_name}")
